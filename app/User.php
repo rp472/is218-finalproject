86c098a38284793a61d6b07edf599c5c8dfc69dd
raj <rp472@njit.edu>
2018-08-08 13:05:36 -0400

    Added Question,  migration model, and updated the user seeder to create profiles and added test for userprofile(cleanup) and question tests

--- app/User.php
+++ app/User.php
@@ -53,4 +53,10 @@ class User extends Authenticatable
         return $this->hasOne('App\Profile');
     }
 
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
+
+
 }
